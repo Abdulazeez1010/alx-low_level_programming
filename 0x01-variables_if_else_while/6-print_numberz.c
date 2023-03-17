ae021b2397d22f078a8209f356bcbea1b7eb90ec
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-17 15:01:12 -0700

    Added the print_numberz file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ * of base 10 starting from 0
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+        while (i <= '9')
+	{
+		putchar(i);
+	       	i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
