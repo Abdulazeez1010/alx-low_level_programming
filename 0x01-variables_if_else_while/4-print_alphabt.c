e3d05e234aa25ea26f84ec095081afcb0f478ea9
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-17 14:26:50 -0700

    Added the print_alphabt file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - prints alphabet in lower case
+ * except q and e
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	
+	i = 'A';
+	while (i <= 'Z')
+	{
+		if(i == 'Q' || i == 'E')
+		{
+			i++;
+		} else
+		{
+			putchar(tolower(i));
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
