bfb8dd97bee6fdfd621b7751f11364c441f9e117
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-17 09:10:26 -0700

    Modified the last_digit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,13 +25,13 @@ int main(void)
 	}
 	if (l > 5)
 	{
-		printf("Last digit of %d is greater than 5 and l is %d \n", n, l);
+		printf("Last digit of %d is greater than 5\n", n);
 	} else if (l < 6 && l != 0)
 	{
-		printf("Last digit of %d is less than 6 and not 0and l is %d \n", n, l);
+		printf("Last digit of %d is less than 6 and not 0\n", n);
 	} else if (l == 0)
 	{
-		printf("Last digit of %d is 0 and l is %d \n", n, l);
+		printf("Last digit of %d is 0", n);
 	}
 	return (0);
 }
