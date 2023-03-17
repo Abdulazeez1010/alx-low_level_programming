59f586156609d645fc3488683aee8fe81885474d
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-16 21:49:57 -0700

    Modified the quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*main - Prints a lline to standard error
- *Return: Returns 1
+/**
+ * main - Prints a line to standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      stdout);
 	return (1);
 }
