227edfd251cd9051c8cb4bbcc4af5de51d9af69a
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-17 16:07:08 -0700

    Added the print_base16 file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base-16 
+ * in lower case
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i, j;
+
+	i = '0';
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	
+	j = 0;
+	while (j <= 5)
+	{
+
+		putchar(tolower(65 + j));
+		j++;
+	}
+	putchar('\n');
+
+	return (0);
+}
