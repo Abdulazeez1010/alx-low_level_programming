e07d65ac9a44337cfe7d050d9a418f0dd9c24f5a
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-17 09:17:52 -0700

    Modified the last_digit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	if (l > 0)
-	{
-		l = l;
-	} else if (l < 0)
-	{
-		l = -l;
-	}
 	if (l > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
