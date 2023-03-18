20c2708a11ddf2b05aacb80c535d28c8e1c60069
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-18 10:36:17 -0700

    Modified the print_comb5 file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,44 +3,65 @@
 
 /**
  * main - prints all possible combinations of
- * two two digit numbers
+ * two two digits
  * Return: returns 0
  */
+
 int main(void)
 {
-	int i, j, m, n;
-	i = 0;
-	j = 0;
-	m = 0;
-	n = 0;
-	while (i <= 9)
-	{
-		while (j <= 9)
-		{
-		
-			while (m <= 9)
-				{
-					n =  j + 1;
-					while (n <= 9)
-					{
-						putchar('0' + i);
-						putchar('0' + j);
-						putchar(' ');
-						putchar('0' + m);
-						putchar('0' + n);
-						putchar(',');
-						putchar(' ');
-						n++;
+	int b;
 
+	b = 0;
+	while (b <= 9)
+	{
+		int a;
+
+		a = 0;
+		while(a <= 9)
+		{
+			int i, j, k, n;
+
+			i = 0;
+			j = 0;
+			k = 0;
+			n = 0;
+			i = b;
+			while (i <= 9)
+			{
+				j = a;
+				while (j <= 9)
+				{
+					while (k <= 9)
+					{
+						n = j + 1;
+						while (n <= 9)
+						{
+							putchar('0' + i);
+							putchar('0' + j);
+							putchar(' ');
+							putchar('0' + k);
+							putchar('0' + n);
+							if (!((i == 9) && (j == 8) && (k == 9) && (n == 9)))
+							{
+								putchar(',');
+								putchar(' ');
+								n++;
+							}
+							else
+							{
+								n++;
+							}
+						}
+						k++;
 					}
-					m++;
+					j++;
 				}
-				j++;
-                }
-		i++;
-		m = i;
+				i++;
+			}
+			a++;
+		}
+		b++;
 	}
 	putchar('\n');
-
 	return (0);
 }
