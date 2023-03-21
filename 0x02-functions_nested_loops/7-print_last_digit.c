6fec1ef2fa59c0d812c2b2f57a895f65d0beda2b
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-21 13:56:40 -0700

    Added the last_digit file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: The number to be checked
+ *
+ * Return: absolute value
+ */
+int print_last_digit(int x)
+{
+	int l;
+
+	l = x % 10;
+	return (l);
+}
+
