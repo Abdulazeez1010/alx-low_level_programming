95bce0469cf00d762060a9ee82c4fd2710f72489
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-21 10:16:54 -0700

    Added the 1-alphabet file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
-#include <ctype.h>
+#include "main.h"
 
 /**
- *
- *
+ * print_alphabet - prints alphabet in lower case
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for (i  = 'A'; i <= 'Z'; i++ )
+	for (i  = 'A'; i <= 'Z'; i++)
 	{
 		_putchar(tolower(i));
 	}
-	return (0);
 }
