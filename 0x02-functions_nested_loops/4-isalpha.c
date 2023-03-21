6a37c9df18855a41ab85a5efb1eeb4beaac0c006
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-21 11:45:32 -0700

    Added the isalpha file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "ctype.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The variable to be checked
+ *
+ * Return: 1 if lower. 0 otherwise
+ */
+int _isalpha(int c)
+{
+        if ((c >= 48 && c <= 57) || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+                return (1);
+        }
+        else
+                return (0);
+}
