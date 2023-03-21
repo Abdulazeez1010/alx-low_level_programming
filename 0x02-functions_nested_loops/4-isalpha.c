5f96ed395ec168d05ca3a82a93ab225109e9c0ae
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-21 11:54:22 -0700

    Modified the isalpha file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,14 +5,15 @@
  * _isalpha - checks for alphabetic character
  * @c: The variable to be checked
  *
- * Return: 1 if lower. 0 otherwise
+ * Return: 1 if truei. 0 otherwise
  */
 int _isalpha(int c)
 {
-        if ((c >= 48 && c <= 57) || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        {
-                return (1);
-        }
-        else
-                return (0);
+	if ((c >= '48' && c <= '57') ||
+	(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
