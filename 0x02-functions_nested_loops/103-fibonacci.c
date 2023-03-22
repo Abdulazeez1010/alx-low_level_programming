ae470e8e6519191016a13718ae79013d4b09b56a
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-21 20:28:22 -0700

    Added the 103-fibonacci file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of even fibonacci less than 4000000
+ * Return: return 0 is successful
+ */
+int main(void)
+{
+	long long int i, j, k, sum, even_sum;
+
+	j = 0;
+	k = 1;
+	sum = 0;
+	even_sum = 0;
+	for (i = 0; i < 50; i++)
+	{
+		sum = j + k;
+		j = k;
+		k = sum;
+		if (sum >= 4000000)
+			break;
+		if (sum % 2 == 0)
+			even_sum = even_sum + sum;
+	}
+	printf("%lld", even_sum);
+	printf("\n");
+	return (0);
+}
