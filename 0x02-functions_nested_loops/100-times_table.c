3a94d9cd10e4f01919373b37dd8e4c9c9c6ac100
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-22 19:52:34 -0700

    Modofied the 100-times table file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,69 +1,68 @@
 #include <stdio.h>
 
-/*
- *
+/**
+ * print_times_table - prints the n times table, starting with 0 and max of 15
  */
 void print_times_table(int n)
 {
 	if (n > 0 && n <= 15)
 	{
-	int i, j, r;
+		int i, j, r;
 
-	for (i = 0; i <= n; i++)
-	{
-		for(j = 0; j <= n; j++)
+		for (i = 0; i <= n;
+		i++)
 		{
-			r = i * j;
-			if (i == 0)
+			for (j = 0;
+			j <= n; j++)
 			{
-				printf("%d", r);
-				if (j != n)
+				r = i * j;
+				if (i == 0)
 				{
-					printf(",   ");
+					printf("%d", r);
+					if (j != n)
+					{
+						printf(",   ");
+					}
 				}
-			}
-			if (r == 0 && j == 0 && i != 0)
-			{
-				printf("%d,", r);
-			}
-			if (r >= 1 && r <= 12)
-			{
-				if (r <= 9)
+				if (r == 0 && j == 0 && i != 0)
 				{
-					printf("   ");
+					printf("%d,", r);
 				}
-				if (r > 9)
+				if (r >= 1 && r <= 12)
 				{
-					printf("  ");
+					if (r <= 9)
+					{
+						printf("   ");
+					}
+					if (r > 9)
+					{
+						printf("  ");
+					}
+					printf("%d", r);
+					if (j != n)
+					{
+						printf(",");
+					}
 				}
-				printf("%d", r);
-				if (j != n)
+
+				else if (r > 12)
 				{
-					printf(",");
-				}
-			}
-			else if (r > 12)
-			{
 				if (r <= 99)
 				{
-					printf("  ");
+				printf("  ");
 				}
-				else if(r > 99)
+				else if (r > 99)
 				{
-					printf(" ");
+				printf(" ");
 				}
-				printf("%d", r);
-				if (j != n)
-				{
-					printf(",");
+					printf("%d", r);
+					if (j != n)
+					{
+						printf(",");
+					}
 				}
 			}
-
-
-
+			printf("\n");
 		}
-		printf("\n");
-	}
-	printf("\n");
 	}
 }
