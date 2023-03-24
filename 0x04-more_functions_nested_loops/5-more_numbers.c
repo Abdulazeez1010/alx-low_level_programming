3f7ae1003594ea13f926d73dc9adca057212e346
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-24 03:06:31 -0700

    Modified the 5-more_numbers file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,7 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
@@ -19,5 +19,4 @@ void more_numbers(void)
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
