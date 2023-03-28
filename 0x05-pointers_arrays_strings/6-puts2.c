9608a923d1ec6c788fd9d7d3077e50df760cbf05
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-27 18:03:19 -0700

    Modified the puts2 file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,32 @@
 #include "main.h"
 
 /**
+ * str_len - determines the length of  a string
+ * @ptr: string to determine its length
+ * Return: Length
+ */
+int str_len(char *ptr)
+{
+	int i = 0;
+
+	while (*(ptr + i) != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * puts2 - prints every other character of a string,
  * starting with the first character
+ * @str: string to print every other character
  */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
+
+	len = str_len(str);
 
 	i = 0;
-	while (*(str + i) != '\0')
+	while (i < len)
 	{
 		_putchar(*(str + i));
 		i += 2;
