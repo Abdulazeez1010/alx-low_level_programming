4cccb0e1fe6775bfc63370638fccf95924759c43
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-30 07:26:46 -0700

    Modified the 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * str_len - calculates the length of a string
- * @ptr: string ot determine the length
- * Return: returns length of string
- */
-int str_len(char *ptr)
-{
-	int i;
-
-	i = 0;
-	while (ptr[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
@@ -29,28 +11,15 @@ int str_len(char *ptr)
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	int len1 = str_len(s1);
-	int len2 = str_len(s2);
-
-	if (len1 < len2)
-		return (-15);
-	if (len2 < len1)
-		return (15);
-
-	for (i = 0; s2[i] != '\0'; i++)
+	while (*s1)
 	{
-		if ((len1 == len2) && (s1[i] == s2[i]))
-			return (0);
-		else if (s1[i] != s2[i])
+		if (*s1 != *s2)
 		{
-			if (s1[i] < s2[i])
-				return (-15);
-			if (s2[i] < s1[i])
-				return (15);
+			break;
 		}
+		s1++;
+		s2++;
 	}
 
-	return (0);
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
 }
