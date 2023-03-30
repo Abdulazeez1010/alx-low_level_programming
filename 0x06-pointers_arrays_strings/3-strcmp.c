d9ffde765cff46f1e9856e7c8b033c53a4a35689
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-30 07:16:36 -0700

    Modified the strcmp file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -43,6 +43,13 @@ int _strcmp(char *s1, char *s2)
 	{
 		if ((len1 == len2) && (s1[i] == s2[i]))
 			return (0);
+		else if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (-15);
+			if (s2[i] < s1[i])
+				return (15);
+		}
 	}
 
 	return (0);
