957bc870dd6cadb2f4b0c5e8d0ded8f388f30590
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-30 14:49:12 -0700

    Added the 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using ROT13
+ * @s: string to encode
+ * Return: returns encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rotcode = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = rotcode[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
