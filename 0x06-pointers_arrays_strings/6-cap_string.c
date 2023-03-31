c28946abf8d771aa459e7a8ac8c5da8a2df90a65
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-30 18:23:18 -0700

    Modified the 6-cap_string file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
  * @s: string to capitalize
  * Return: Returns string
  */
-char *cap_string(char *s)
+/*char *cap_string(char *s)
 {
 	int i, j;
 
@@ -13,9 +13,9 @@ char *cap_string(char *s)
 	while (s[i] != '\0')
 	{
 		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
-			|| s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?'
-			|| s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == 123
-			|| s[i] == 125)
+                        || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+                        || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{'
+                        || s[i] == '}' || i - 1 == 0)
 		{
 			for (j = 'a'; j <= 'z'; j++)
 			{
@@ -28,4 +28,41 @@ char *cap_string(char *s)
 		i++;
 	}
 	return (s);
+} */
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
 }
+
