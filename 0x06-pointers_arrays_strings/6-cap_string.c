5ebeffac1ffd945fc210b1d6454316e44f54299b
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-03-30 18:27:33 -0700

    Modified the 6-cap_string file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
  * @s: string to capitalize
  * Return: Returns string
  */
-/*char *cap_string(char *s)
+char *cap_string(char *s)
 {
 	int i, j;
 
@@ -22,47 +22,11 @@
 				if (s[i + 1] == j)
 				{
 					s[i + 1] = s[i + 1] - 32;
+					break;
 				}
 			}
 		}
 		i++;
 	}
 	return (s);
-} */
-/**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
- */
-char *cap_string(char *str)
-{
-	int index = 0;
-
-	while (str[index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
-	}
-
-	return (str);
 }
-
