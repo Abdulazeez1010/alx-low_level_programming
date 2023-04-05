a87b5d84cc58f0621aab36696055a723b402d8bd
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-05 15:53:41 -0700

    Added the 4-pow_recursion file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - The function returns the value of a number
+ * raised to the power of another number
+ * @x : Number to raise to another number
+ * @y : The exponent
+ * Return : Returns the value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	y--;
+	return (x * _pow_recursion(x, y));
+}
