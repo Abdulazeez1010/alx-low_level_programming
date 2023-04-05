2a5bb614f673ec71377a9efdd0a696eded8a2b9f
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-05 15:22:01 -0700

    Fixed betty errors

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * factorial - The function returns the factorial of a given number
+ * @n : Number to determine its factorial
+ * Return: Returns factorial
+ */
 int factorial(int n)
 {
 	if (n < 0)
