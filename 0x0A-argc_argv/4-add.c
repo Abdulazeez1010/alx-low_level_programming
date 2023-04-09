3db0569fdd6aa965cee8fb0a67a4cfe61eed686c
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-09 07:03:30 -0700

    Added the 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - The function adds positive numbers
+ * @argc : argument counter
+ * @argv : argument vector
+ * Return: Return 1 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+
+	if (argc > 2)
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) > 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	/*printf("%d\n", sum);*/
+	return (0);
+}
