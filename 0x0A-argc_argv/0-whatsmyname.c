91e7eef5b6be0efbb7f83fe8d5de509be2a30c96
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-09 04:47:43 -0700

    Modified the 0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
  * main - The function prints its name followed by a new line
  * @argc : argument count
  * @argv : argument vector
- * Return : 0 if successful
+ * Return: 0 if successful
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
