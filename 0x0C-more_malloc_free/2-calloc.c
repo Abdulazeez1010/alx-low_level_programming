4f5c85031bdaadfaa877a0e39d646277c4dc7b9d
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-13 10:08:56 -0700

    Added the 2-calloc.c file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(unsigned int) * (nmemb + size));
+	arr = malloc(nmemb * sizeof(size));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
