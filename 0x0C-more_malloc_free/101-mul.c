5b6bfd1bfb578c20b42a1302c5c9a6b7dcb2e1eb
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-13 21:53:28 -0700

    Added the 101ded-mul.c file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - The function multiplies two positive numbers
+ * @argc : argument counter
+ * @argv : argument vector
+ * Return: Return 0 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	long int prod;
+
+	if (argc == 3)
+	{
+		char *endptr;
+		long int n1 = strtol(argv[1], &endptr, 10);
+		long int n2 = strtol(argv[2], &endptr, 10);
+
+		if (*endptr != '\0')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		if (n1 < 0 || n2 < 0)
+		{
+			return (1);
+		}
+		prod = n1 * n2;
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%ld\n", prod);
+	return (0);
+}
