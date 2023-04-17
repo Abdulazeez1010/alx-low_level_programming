d928ea44e7e2f88d3a0a6d74b8bb5d8f728a5a78
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-17 16:01:40 -0700

    Added the 2-main.c file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - The program prints the name of the file it was compiled from,
+ * followed by a new line
+ * Return: 0 if succesful
+ */
 int main (void)
 {
 	printf("%s\n", __BASE_FILE__);
