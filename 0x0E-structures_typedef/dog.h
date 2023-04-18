a8f247c5f9024b9e20a3b26741d51a8384080dd2
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-04-17 18:01:45 -0700

    Added the dog.h and the README.md files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - A new struct type called dog
+ * @name : Name of dog
+ * @age : Age of dog
+ * @owner : Owner of og
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* ifndef DOG_H*/
