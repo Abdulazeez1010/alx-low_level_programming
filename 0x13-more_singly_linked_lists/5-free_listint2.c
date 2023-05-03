eaed528d8e9cc69af9b5b4e1c9beb1efe1cb6ceb
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-05-02 17:24:19 -0700

    Modified the 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,8 @@ void free_listint2(listint_t **head)
 	listint_t *current = *head;
 	listint_t *next;
 
-	*head = NULL;
+	if (*head == NULL)
+		return;
 
 	while (current != NULL)
 	{
@@ -17,4 +18,5 @@ void free_listint2(listint_t **head)
 		free(current);
 		current = next;
 	}
+	*head = NULL;
 }
