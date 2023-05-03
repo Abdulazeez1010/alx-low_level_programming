637522d1c7d4734f1048724caedfc1cadaa6449d
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-05-03 10:00:51 -0700

    Modified the 6-pop_listint.c file

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,12 +8,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp = *head;
-	int n = temp->n;
+	int n;
 
 	if (*head == NULL)
 		return (0);
 	if (!temp)
 		return (0);
+	n = temp->n;
 	*head = (*head)->next;
 	free(temp);
 	temp = NULL;
