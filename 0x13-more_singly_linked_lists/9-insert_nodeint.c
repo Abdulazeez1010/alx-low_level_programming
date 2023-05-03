9c37ffae121677b68aef79308d3d5faa1ae17127
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-05-02 19:39:43 -0700

    Modified the 9-insert_nodeint.c file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (ptr == NULL)
-		return (NULL)i;
+		return (NULL);
 
 	if (ptr2 == NULL)
 		return (NULL);
