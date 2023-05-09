79396d473223ef9bd948e455380d658a5ce1e7bb
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-05-08 19:57:44 -0700

    Modified the 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,22 +9,33 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fd = fopen(filename, "r");
-	size_t bytes_read;
-	char *buffer = malloc(letters + 1);
+	int fd;
+	ssize_t bytes_read;
+	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
+	{
+		close(fd);
 		return (0);
-	if (fd == NULL)
-		return (0);
+	}
 
-	bytes_read = fread(buffer, sizeof(char), letters, fd);
-	if (bytes_read == 0)
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 	buffer[bytes_read] = '\0';
 	printf("%s", buffer);
 
-	fclose(fd);
+	close(fd);
 	free(buffer);
 	return (bytes_read);
 }
