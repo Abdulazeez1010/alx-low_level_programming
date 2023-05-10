ac1d8699ef6af678146c4ce5b0ab15851662be14
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-05-10 07:33:34 -0700

    Modified the 3-cp.c file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,32 +1,5 @@
 #include "main.h"
 
-
-/**
- * inti_err_handle - Handles the initial errors in main function
- * @argc : Argument count
- * @argv : Argument vector
- */
-void init_err_handle(int argc, char *argv[])
-{
-	int file_from = open(argv[1], O_RDONLY);
-	int file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-		exit(97);
-	}
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
-	}
-}
 /**
  * main - The program copies the content of a file to another file
  * @argc : Argument count
@@ -44,6 +17,16 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
+	}
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
 	}
 	while ((bytes_read = read(file_from, buffer, 1024)) > 0)
 	{
