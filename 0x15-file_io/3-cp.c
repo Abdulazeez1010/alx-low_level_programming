078c2d721fdb3d0f7d7d738d99e489c304be8df6
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-05-12 03:44:12 -0700

    Modified the 3-cp.c file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,24 +10,22 @@ int main(int argc, char *argv[])
 {
 	char buffer[BUFFER];
 	ssize_t bytes_read, bytes_written;
-	int file_from = open(argv[1], O_RDONLY);
-	int file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	int file_from;
+	int file_to;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (file_from == file_to)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
@@ -35,18 +33,8 @@ int main(int argc, char *argv[])
 	}
 	while ((bytes_read = read(file_from, buffer, sizeof(buffer))) > 0)
 	{
-		if (bytes_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
 		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			exit(99);
-		}
-		if (bytes_written != bytes_read)
+		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			exit(99);
