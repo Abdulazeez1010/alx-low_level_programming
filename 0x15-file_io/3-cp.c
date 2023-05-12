e0e3fcc5f2bcdde18321b1218d398989c3abf882
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-05-12 03:56:21 -0700

    Modified the 3-cp.c file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,6 +18,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	if (strcmp(argv[1], argv[2]) == 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Input and output files are the same\n");
+		exit(97);
+	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
