00684ad28ad42960585675bd46403a4bfb3e155d
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-06-14 21:32:49 -0700

    Modified the 2-str_concat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * str_len - Calculates the length of a string
- * @s : The string to calculate its length
- * Return: Returns length of the string
- */
-int str_len(char *s)
-{
-	int len;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-
-/**
  * str_concat - The function concatenates two strings
  * @s1 : String 1
  * @s2 : String 2
@@ -25,7 +8,7 @@ int str_len(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2;
+	int i, j, len1, len2, len;
 	char *str;
 
 	len1 = 0;
@@ -36,12 +19,11 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	if (s1 != NULL)
-		len1 = str_len(s1);
-	if (s2 != NULL)
-		len2 = str_len(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	len = len1 + len2;
 
-	str = malloc(sizeof(char) * (len1 + len2 + 1));
+	str = malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
