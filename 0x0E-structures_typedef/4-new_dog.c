c04874389d0653160dd718f6cff5ceb11e21abe7
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-06-19 19:47:52 -0700

    Modified the 4-new_dog.c file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,24 +10,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *name_copy, *owner_copy;
-
-	name_copy = strdup(name);
-	if (name_copy == NULL)
-		return (NULL);
-
-	owner_copy = strdup(owner);
-	if (owner_copy == NULL)
-	{
-		free(name_copy);
-		return (NULL);
-	}
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	d->name = malloc(strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	strcpy(d->name, name);
+
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	strcpy(d->owner, owner);
+
 	d->age = age;
-	d->owner = owner;
 	return (d);
 }
