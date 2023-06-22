3c923dcb5b1dfe18ac024d790a8fae04fa8fb1cf
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-06-21 19:59:44 -0700

    Added the 3-calc.h, 3-get_op_func.c, 3-main.c, and 3-op_functions.c files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * main - The main function
+ * @argc : Argument count
+ * @argv : Argument vector
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 &&
+			strcmp(argv[2], "*") != 0 && strcmp(argv[2], "/") != 0
+			&& strcmp(argv[2], "%") != 0)
+	{
+		printf("%s", argv[2]);
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "&") == 0) &&
+			(atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Addition function
+ * @a : First operand
+ * @b : Second operand
+ * Return: Returns sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtraction function
+ * @a : First operand
+ * @b : Second operand
+ * Return: Returns subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplication function
+ * @a : First operand
+ * @b : Second operand
+ * Return: Returns product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division function
+ * @a : First operand
+ * @b : Second operand
+ * Return: Returns quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulo function
+ * @a : First operand
+ * @b : Second operand
+ * Return: Returns remainder from division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
