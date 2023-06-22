59e6d12d01fdc105b3274e90653f8cf70704bc17
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-06-21 22:46:14 -0700

    Modified the 100-mai_opcodes.c file

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
 	num_bytes = atoi(argv[1]);
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
 	for (i = 0; i < num_bytes; i++)
 		printf("%02x ", op_codes[i]);
