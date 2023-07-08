2e34bb6eafb15c4c4a6e97682ff22f555c5112cb
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-07-07 18:28:37 -0700

    Added the 1-print_binary.c file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
