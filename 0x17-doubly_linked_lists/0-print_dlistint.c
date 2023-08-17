af1e2d4496975c9abf6674f0d73e47313784fd8e
Abdulazeez <alani.abdulazeez.akinpelumi@gmail.com>
2023-08-17 15:51:14 -0700

    Fixed Betty issue

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
+ * print_dlistint - The function prints all the elements of a dlistint_t list
+ * @h : The head pointer
+ * Return: Returns number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
